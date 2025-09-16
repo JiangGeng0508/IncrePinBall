461d8ee28ae74aa16da03272b479a5208990987f
ZinC0508 <18335432463@163.com>
2025-09-16 10:38:31 +0800

    WeakWall

--- Cs/WeakWall.cs
+++ Cs/WeakWall.cs
@@ -1,13 +1,36 @@
 using Godot;
 using System;
 
-public partial class WeakWall : RigidBody2D
+public partial class WeakWall : StaticBody2D
 {
+    [Export] public int Health = 3;
+    private int _health;
+    public override void _Ready()
+    {
+        _health = Health;
+    }
+
     public void OnBodyEntered(Node body)
     {
         if (body is Ball)
         {
-            GD.Print("WeakWall hit");
+            //脆弱的墙壁被撞击
+            _health--;
+            GD.Print("WeakWall hitted");
+            if (_health <= 0)
+            {
+                CallDeferred(nameof(Destroy));
+            }
+            else if (_health < Health)
+            {
+                GetNode<ColorRect>("ColorRect").Color = new Color((float)_health / Health,0,0,1);
+            }
+            else
+            {
+                GetNode<ColorRect>("ColorRect").Color = Colors.White;
+            }
         }
     }
+
+    private void Destroy() => QueueFree();
 }
